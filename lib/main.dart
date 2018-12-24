cda0f0b100f8bdff9fd53f1538f323a92bde709c
huangmingchao <huangmingchao0312@gmail.com>
2018-12-24 18:01:34 +0800

    路径修改;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'pages/login.dart';
+import 'package:flutterapp/pages/login.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
